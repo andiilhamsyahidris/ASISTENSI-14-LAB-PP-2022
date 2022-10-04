3c4048cc3815e4129d3ded9a9f6a843820c833e2
muhammadfadhilabdillah <muhabdillah63@gmail.com>
2022-10-04 10:57:25 +0800

    Tugas Pertemuan 2

---
+++ H071221097/Pertemuan-1/assignment_1_no3.py
@@ -0,0 +1,7 @@
+#menghitung persenan gaji
+print(input('masukkan nama:'))
+a = float(input ('gaji pokok:'))
+b = float(input('total penjualan:'))
+c = (b * 0.015)
+d = (a + c)
+print('TOTAL = $ %s'%(d))
