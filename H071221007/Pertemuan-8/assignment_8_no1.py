b19d7b109ca4e910a38cb81c91cb92939d830d39
muh-iswari <iswarihasis@gmail.com>
2022-11-09 20:48:33 +0800

    Tugas Pertemuan 8

---
+++ H071221007/Pertemuan-8/assignment_8_no1.py
@@ -0,0 +1,40 @@
+class person :
+    def __init__(self, nama, umur, laki_laki) :
+        #print("="*10,)
+        self.nama = nama 
+        self.umur = umur
+        self.laki_laki = laki_laki
+    def setAge (self, umur) :
+        self.umur = umur
+    def getAge (self) :
+        print(f"Umur   : {self.umur} Tahun")
+    def setName (self, nama) :
+        self.nama = nama
+    def getName (self) :
+        print(f"Nama   : {self.nama}")
+    def setGender (self, laki_laki) :
+        self.laki_laki = laki_laki
+    def getGender (self) :
+        if self.laki_laki == 'True' or self.laki_laki == 'true':
+            self.laki_laki = True
+            if self.laki_laki == True :
+                print("Gender : Laki-laki")
+        elif self.laki_laki == 'False' or self.laki_laki == 'false':
+            self.laki_laki = False
+            if self.laki_laki == False :
+                print("Gender : Perempuan")
+        else :
+            print("inputan tidak falid")
+    #def Kesehatan (self) :
+        #pass
+
+
+
+name = input()
+age = int(input())
+isMale = input()
+human = person(name, age, isMale)
+
+human.getName()
+human.getAge()
+human.getGender()
