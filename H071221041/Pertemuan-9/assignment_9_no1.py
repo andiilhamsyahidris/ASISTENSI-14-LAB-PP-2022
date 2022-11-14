d3d2ed48ea67b83134fcd47b3fe9c66c931371ee
andimuthia <andimuthiamlptr@gmail.com>
2022-11-14 16:39:36 +0800

    Tugas Pertemuan 9

---
+++ H071221041/Pertemuan-9/assignment_9_no1.py
@@ -0,0 +1,36 @@
+from hero import Warrior, Assassin, Support
+warrior = Warrior("bambang", pos_x=10)
+assassin = Assassin("joko", pos_x=25)
+support = Support("udin",pos_x=30)
+# sebelum
+print("health (before)", warrior.getHealth())
+assassin.attack(warrior)
+# sesudah
+print("health (after)", warrior.getHealth())
+print("-"*10)
+# sebelum
+print("Warrior (health)", warrior.getHealth())
+print("Support (speed) : ",support.getSpeed())
+support.special(warrior)
+# sesudah
+print("Warrior (health)", warrior.getHealth())
+print("Support (speed): ",support.getSpeed())
+
+print(warrior.getMovement())
+warrior.setMovement("L")
+print(warrior.getMovement())
+print("_"*10)
+print(warrior.getMovement())
+
+print(assassin.getMovement())
+assassin.setMovement("L")
+print(assassin.getMovement())
+print("_"*10)
+print(assassin.getMovement())
+
+
+print(support.getMovement())
+support.setMovement("L")
+print(support.getMovement())
+print("_"*10)
+print(support.getMovement())
